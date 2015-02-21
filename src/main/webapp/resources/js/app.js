19ec704213f74080ef64dcff55d392462dc46135
Nikiforov Denis <denak.nikiforov@ya.ru>
2015-02-21 23:48:28 +0200

    Созданы страницы авторизации и регистрации

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -4,6 +4,14 @@ taskerApp.config(['$routeProvider','$locationProvider',
     function($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
+                templateUrl: '/resources/templates/login.html',
+                controller: 'TasksController'
+            })
+            .when('/register', {
+                templateUrl: '/resources/templates/register.html',
+                controller: 'TasksController'
+            })
+            .when('/tasks', {
                 templateUrl: '/resources/templates/tasks.html',
                 controller: 'TasksController'
             })
