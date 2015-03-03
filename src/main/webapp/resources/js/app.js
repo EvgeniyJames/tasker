2f48201698372e36d7cbb5e6b0e34b797410cd0a
Nikiforov Denis <denak.nikiforov@ya.ru>
2015-03-03 19:27:11 +0200

    Востановил потеряные даные

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -4,10 +4,29 @@ taskerApp.config(['$routeProvider','$locationProvider',
     function($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
+                templateUrl: '/resources/templates/login.html'
+            })
+            .when('/register', {
+                templateUrl: '/resources/templates/register.html'
+            })
+            .when('/tasks', {
                 templateUrl: '/resources/templates/tasks.html',
-                controller: 'UsersController'
+                controller: 'TasksCtrl'
+            })
+            .when('/me', {
+                templateUrl: '/resources/templates/me.html',
+                controller: 'MeCtrl'
+            })
+            .when('/user', {
+                templateUrl: '/resources/templates/user.html',
+                controller: 'UserCtrl'
+            })
+            .when('/friends', {
+                templateUrl: '/resources/templates/friends.html',
+                controller: 'FriendsCtrl'
             })
             .otherwise({
                 redirectTo: '/'
             });
+
     }]);
